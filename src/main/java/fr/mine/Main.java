ef8bdedd2b1d2ddfba1d68a2902cc89e76b57404
Mine <thomaschassang6@gmail.com>
2023-05-24 19:09:27 +0200

    onLoad onDisable

--- src/main/java/fr/mine/Main.java
+++ src/main/java/fr/mine/Main.java
@@ -1,5 +1,20 @@
 package fr.mine;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    @Override
+    public void onEnable() {
+    }
+
 
 }
